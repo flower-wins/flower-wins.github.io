4a8a7465c21a35860d6b72156bd7681ce2fee4e5
花得胜 <1107928521@qq.com>
2025-01-20 02:25:54 +0800

    Site updated: 2025-01-20 02:25:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["fws/这是一篇新的博文/","fws/这是另一篇新的博文/","fws/hello-world/"];function toRandomPost(){
+var posts=["fws/这是另一篇新的博文/","fws/这是一篇新的博文/","fws/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
