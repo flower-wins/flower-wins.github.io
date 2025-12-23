aedfa0476128e08187861dd9781993ccf3f811a4
flower-wins <huadesheng@proton.me>
2025-12-24 01:06:00 +0800

    Site updated: 2025-12-24 01:04:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["fws/HiDNS域名终身免费计划/","fws/Mouse-Without-Borders/","fws/神站合集发布版/","fws/VPS-v/","fws/GPT-ubuntu-install-npm/","fws/部署Twikoo/","fws/Syncthing/","fws/免费节点/","fws/acme-sh/","fws/FileBrowser/","fws/3x-ui面板搭建/","fws/RackNerd-Hysterial2/","fws/yg-workers-keep/","fws/HexoBlogNo2/","fws/HexoBlogNo1/","fws/WebSSH一键部署/","fws/hello-world/"];function toRandomPost(){
+var posts=["fws/Mouse-Without-Borders/","fws/HiDNS域名终身免费计划/","fws/神站合集发布版/","fws/VPS-v/","fws/GPT-ubuntu-install-npm/","fws/部署Twikoo/","fws/Syncthing/","fws/免费节点/","fws/acme-sh/","fws/FileBrowser/","fws/3x-ui面板搭建/","fws/RackNerd-Hysterial2/","fws/yg-workers-keep/","fws/HexoBlogNo2/","fws/HexoBlogNo1/","fws/WebSSH一键部署/","fws/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
